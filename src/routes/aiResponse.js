a127ae5d5ae51668e821d11f9a1078324cd6f92b
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2025-01-29 16:22:08 +0100

    Update aiResponse.js

--- src/routes/aiResponse.js
+++ src/routes/aiResponse.js
@@ -13,12 +13,17 @@ router.post('/generate-response', async (req, res) => {
             return res.status(400).json({ error: "API Key OpenAI mancante!" });
         }
 
-        const response = await axios.post("https://api.openai.com/v1/chat/completions", {
+      const dataOpenAi = {
             model: "gpt-4o-mini",
             messages: [{ role: "system", content: "Rispondi in modo professionale." }, { role: "user", content: userMessage }],
             temperature: settings.ai.temperature || 0.7,
             max_tokens: settings.ai.maxTokens || 200
-        }, {
+        }
+
+       console.log('test invio a https://api.openai.com/v1/chat/completions');
+       console.log('openai settings.ai.openAiKey', settings.ai.openAiKey);
+
+        const response = await axios.post("https://api.openai.com/v1/chat/completions", dataOpenAi, {
             headers: { Authorization: `Bearer ${settings.ai.openAiKey}`, "Content-Type": "application/json" }
         });
 
