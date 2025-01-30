96a122f41bb15db19a1e6694a77a733d79e610d6
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2025-01-30 14:08:06 +0100

    Update aiResponse.js

--- src/routes/aiResponse.js
+++ src/routes/aiResponse.js
@@ -26,7 +26,7 @@ router.post('/generate-response', async (req, res) => {
                 const response = await axios.post("https://api.openai.com/v1/chat/completions", dataOpenAi, {
                     headers: { Authorization: `Bearer ${settings.ai.openAiKey}`, "Content-Type": "application/json" }
                 });
-                  console.log('xxxxxxxxxxxxxx, response.data);
+                  console.log('xxxxxxxxxxxxxx', response.data);
 
                 const aiData = response.data; // .choices[0].message.content;
                 if (!aiData || !aiData.choices[0] || !aiData.choices[0].message){
