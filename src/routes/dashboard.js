bbf1884be568efe9f040d3699522416616eccb92
Daniele Tosoni <danieletosoni@gmail.com>
2025-02-04 19:12:47 +0100

    creato layout

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = express.Router();
 console.log('sono su route dashboard')
 router.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/admin/dashboard.html'));
+    res.sendFile(path.join(__dirname, '../views/admin/index.html'));
 });
 
 module.exports = router;
