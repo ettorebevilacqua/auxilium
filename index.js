ab47e1118fb5384d66d29e03583030bcb040df64
Daniele Tosoni <danieletosoni@gmail.com>
2025-02-06 13:05:56 +0100

    Fix vercel

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.setHeader("X-Frame-Options", "ALLOWALL");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Shopify-Access-Token");
   next();
 });
 
