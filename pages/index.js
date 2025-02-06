61f3719a6dd13bb12201b5922357cdcc50641785
Daniele Tosoni <danieletosoni@gmail.com>
2025-02-06 15:32:21 +0100

    Fix

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>Hello world *</h1>
       <Link href="/dashboard">
         <a>Vai alla Dashboard</a>
       </Link>
