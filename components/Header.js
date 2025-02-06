763014bfbeea47b6bca086c8ee98dc838c5501fa
Daniele Tosoni <danieletosoni@gmail.com>
2025-02-06 15:40:50 +0100

    NEW COMPONETS DIR

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+      <header>
+        <h1>Dashboard Header</h1>
+        {/* Altri elementi dell'header, ad esempio menu o logo */}
+      </header>
+    );
+  }
+  
